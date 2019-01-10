8e0e8b15c1c0df83da6ef2e947a975e2e37f8365
Alex Smith <hoshialisato@gmail.com>
2019-01-10 05:24:32 +0000

    Added a load of stuff its great

--- start.py
+++ start.py
@@ -1,3 +1,4 @@
+import re
 """
 Alex's Input and Output library
 """
@@ -7,6 +8,22 @@ Output will be controlled/ set up by a plain txt file that you can configure, th
 appears in the console!
 """
 #TODO: start working.
-class OPutput:
-    def __init__(self):
-        self.something = "noghting"
+class Output:
+    def __init__(self, templateFile, itemsList):
+        self._templateFile = templateFile
+        self._items = itemsList
+        with open(self._templateFile, 'r') as templateFileOpen:
+            tempVar = templateFileOpen.read()
+            print(tempVar.format(*self._items))
+
+    def __str__(self):
+        return "Template File Name : {}".format(self._templateFile)
+
+    def info(self):
+        return """To use the Output template all you simply have to do is write what you want your output to look like 
+        in a simple .txt file.
+        For adding variables in a spesific place type {} this whole thing just utilises .format"""
+
+if __name__ == '__main__':
+    test1 = Output('testTemplate.txt', [])
+    print(test1)
