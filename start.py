2d2bf0442a2c4a3d93df643e91c61ace9991ecd5
Alex Smith <hoshialisato@gmail.com>
2019-01-10 05:37:29 +0000

    Added a load of stuff its great

--- start.py
+++ start.py
@@ -7,14 +7,11 @@ Alex's Input and Output library
 Output will be controlled/ set up by a plain txt file that you can configure, the layoiut in the txt file will be how it 
 appears in the console!
 """
-#TODO: start working.
 class Output:
-    def __init__(self, templateFile, itemsList):
+    def __init__(self, templateFile):
         self._templateFile = templateFile
-        self._items = itemsList
         with open(self._templateFile, 'r') as templateFileOpen:
-            tempVar = templateFileOpen.read()
-            print(tempVar.format(*self._items))
+            self._template=templateFileOpen.read()
 
     def __str__(self):
         return "Template File Name : {}".format(self._templateFile)
@@ -24,6 +21,16 @@ class Output:
         in a simple .txt file.
         For adding variables in a spesific place type {} this whole thing just utilises .format"""
 
+    def output(self, itemsList):
+        tempVar = self._template
+        print(tempVar.format(*itemsList))
+
 if __name__ == '__main__':
-    test1 = Output('testTemplate.txt', [])
+    test1 = Output('testTemplate.txt')
     print(test1)
+    test1.output([1,2,3,4])
+    test1.output(['pog','absolute','champ','mydoode'])
+
+    test2 = Output('testTemplate2.txt')
+    test2.output(['1.5million euro', 'Veteran in Quake maybe', 'none'])
+    test2.output(['wow would you look at that', 'PRO minecraft', 'mincraft takes skill'])
