55f9e8fb50baa6ea9f683573185675c4e3d24283
Alex Smith <hoshialisato@gmail.com>
2019-01-10 04:32:16 +0000

    Initial commit

---
+++ start.py
@@ -0,0 +1,9 @@
+"""
+Alex's Input and Output library
+"""
+
+"""
+Output will be controlled/ set up by a plain txt file that you can configure, the layoiut in the txt file will be how it 
+appears in the console!
+"""
+#TODO: start working.
